939ba3a719af615b1d738a6c055b4dbcc47e1b46
Mahir Molai <mahir@molai.dev>
2022-12-11 22:15:37 +0500

    push

---
+++ README.md
@@ -0,0 +1,2 @@
+# BLOG
+this is a redo of my old blog & will be now known as blog v2;
---
+++ js/main.js
@@ -0,0 +1,3 @@
+const post1 = function() {
+    location='/posts\/best-programming-languages-to-learn-in-2023\/'
+}
