bce6c46ec3ce40f9eb6b1e70d3f3b62bba3ac5f7
Mahir Molai <mahir@molai.dev>
2022-12-12 18:39:58 +0500

    feat: new article

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 const post1 = function() {
     location='/posts\/best-programming-languages-to-learn-in-2023\/'
 }
+
+const post2 = function() {
+    location='/posts\/tips-for-python-beginners\/'
+}
