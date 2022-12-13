cbdc42b9caac41fa72594c8f17c6afd08c14f1be
Mahir Molai <mahir@molai.dev>
2022-12-13 15:10:03 +0500

    feat: design changes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-const post1 = function() {
-    location='/posts\/best-programming-languages-to-learn-in-2023\/'
+const article1 = function() {
+    location='/articles\/best-programming-languages-to-learn-in-2023\/'
 }
 
-const post2 = function() {
-    location='/posts\/tips-for-python-beginners\/'
+const article2 = function() {
+    location='/articles\/tips-for-python-beginners\/'
 }
 
-const post3 = function() {
-    location='/posts\/an-introduction-to-responsive-web-design\/'
+const article3 = function() {
+    location='/articles\/an-introduction-to-responsive-web-design\/'
 }
 
 const toGithub = function() {
