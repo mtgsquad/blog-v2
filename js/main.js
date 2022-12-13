90a796658fc859943791d4ad7f244d0e0419371d
Mahir Molai <mahir@molai.dev>
2022-12-13 19:29:26 +0500

    feat: new article

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const article1 = function() {
     location='/articles\/best-programming-languages-to-learn-in-2023\/'
 }
-
 const article2 = function() {
     location='/articles\/tips-for-python-beginners\/'
 }
-
 const article3 = function() {
     location='/articles\/an-introduction-to-responsive-web-design\/'
 }
-
+const article4 = function() {
+    location='/articles\/introduction-to-sveltekit\/'
+}
 const toGithub = function() {
     location='https://github.com/mtgsquad'
 }
